e30cba547a0aa87b046a4832ea96a65401f827e6
Rohini-guvi <pkrd.rohini89@gmail.com>
2025-05-17 08:30:33 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,7 +7,7 @@ st.subheader("Objective:")
 st.markdown("""
 - The objective is to develop a user-friendly Streamlit web application that leverages clickstream data to enhance customer engagement and drive sales.
 - Dataset is given from 5 months of 2008 and includes product category, location of the photo on the page, country and product price in US dollars.
-- Predict whether a customer will complete a purchase with 
+- Predict whether a customer will complete the purchase with price above or below the average price of the product.
 - Estimate the potential revenue a customer is likely to generate, helping the business forecast revenue and optimize marketing strategies.
 - Segment customers into distinct groups based on their online behavior patterns, enabling targeted marketing campaigns and personalized product recommendations.
 """)
